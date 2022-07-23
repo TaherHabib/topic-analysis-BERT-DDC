b5884901ff8f2a5f2e0ec39902b813fd4693194c
TaherHabib <33577096+TaherHabib@users.noreply.github.com>
2022-07-23 19:06:06 +0200

    Some changes to BASE topic modeller :: unfinished due to problems with API calls in _build_model() when setting different HF models (BERT or DistilBERT)

--- models/BaseTopicModel.py
+++ models/BaseTopicModel.py
@@ -1,15 +1,21 @@
+import os
 import transformers
 import tensorflow as tf
-from utils.settings import HF_model_name
+from utils.settings import get_data_root, HF_bert_model_name, HF_distilbert_model_name
 from utils.data_utils import original_ddc_loader as odl
 
+data_root = get_data_root()
 
-class BaseTopicModel:
+
+class BaseBERTModel:
     def __init__(self,
                  freeze_bert_layers=True,
                  sequence_max_length=300,
-                 ddc_target_classes=None
+                 ddc_target_classes=None,
+                 bert_model_name=None
                  ):
+
+        self.bert_model_name = bert_model_name
         self.freeze_bert_layers = freeze_bert_layers
 
         # create label lookup table for label assignment from last classification layer
@@ -17,21 +23,34 @@ class BaseTopicModel:
         self.class_position_hash = odl.create_ddc_label_lookup(self.ddc_target_classes)
 
         # create tokenizer and set sequence length:
-        self.tokenizer = transformers.BertTokenizer.from_pretrained(HF_model_name)
+        self.tokenizer = transformers.BertTokenizer.from_pretrained(self.bert_model_name)
         self.max_length = sequence_max_length
+
         self.bert_output, self.model = self._build_model()
 
     def _build_model(self):
-        bert_model = transformers.TFBertModel.from_pretrained(HF_model_name)
-        if self.freeze_bert_layers:
-            bert_model.trainable = False
 
         input_ids = tf.keras.layers.Input(shape=(self.max_length,), name='input_ids', dtype='int32')
         input_masks_ids = tf.keras.layers.Input(shape=(self.max_length,), name='attention_mask', dtype='int32')
         input_type_ids = tf.keras.layers.Input(shape=(self.max_length,), name='token_type_ids', dtype='int32')
-        bert_output = bert_model(input_ids, attention_mask=input_masks_ids, token_type_ids=input_type_ids)
 
-        return bert_output, bert_model
+        if self.bert_model_name == HF_bert_model_name:
+            bert_model = transformers.TFBertModel.from_pretrained(self.bert_model_name)
+            bert_output = bert_model(input_ids, attention_mask=input_masks_ids, token_type_ids=input_type_ids)
+            model = tf.keras.Model(inputs=[input_ids, input_masks_ids, input_type_ids], outputs=bert_output)
+
+        elif self.bert_model_name == HF_distilbert_model_name:
+            bert_model = transformers.TFDistilBertModel.from_pretrained(self.bert_model_name)
+            bert_output = bert_model(input_ids, attention_mask=input_masks_ids)
+            model = tf.keras.Model(inputs=[input_ids, input_masks_ids], outputs=bert_output)
+
+        else:
+            raise ValueError('Wrong model name found. Check available names of models from HuggingFace.')
+
+        if self.freeze_bert_layers:
+            bert_model.trainable = False
+
+        return bert_output, model
 
     def batch_get_pruned_model_output(self, batch_tokenized_data=None, batch_size=None, prune_at_layer=None):
 
@@ -41,7 +60,7 @@ class BaseTopicModel:
         elif prune_at_layer == 'sequence_output':
             pruned_model = tf.keras.Model(inputs=local_model.inputs, outputs=self.bert_output.last_hidden_state)
         else:
-            pruned_model = tf.keras.Model(inputs=local_model.inputs, outputs=local_model.get_layer(name=prune_at_layer).output)
+            raise ValueError('Please enter a valid layer name for the base bert model: pooler_output OR sequence_output')
 
         return pruned_model.predict(x=batch_tokenized_data, batch_size=batch_size)
 
